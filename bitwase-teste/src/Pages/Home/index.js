e006699dd283b8efbf56aaf2afc89bb716cd8993
Louise Josephine <loophinesj@gmail.com>
2020-08-11 17:02:00 -0300

    Adiciona style e lógica da Home

--- bitwase-teste/src/Pages/Home/index.js
+++ bitwase-teste/src/Pages/Home/index.js
@@ -1,9 +1,11 @@
-import React, { Component, Fragment } from 'react'
+import React, { useState, Fragment } from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import Logo from '../img/Logo.png'
 import ImgApp from '../img/img-app-6.png'
+import ImgBack from '../img/Polygon.png'
 import { RiSearchLine, RiGithubLine, RiInstagramLine, RiLinkedinBoxLine, RiMediumLine } from "react-icons/ri";
+import * as S from './styled'
 
 const getRepositories = gql`
 query GetRepositories {
@@ -33,30 +35,34 @@ query GetRepositories {
   }
 `
 
-class handlePesquisa extends Component {
-    render() {
-        console.log(this.props)
+function App(props) {
 
-        return(
-        <Fragment>
-            <div>
-                <img src={ Logo } alt="logo da Bitwase"></img>
-            </div>
-            <div>
-                <h1>Buscar um usuário no Github ficou muito fácil!</h1>
-                <input placeholder="Buscar usuário"></input>
-                <RiSearchLine />
-                <button><RiGithubLine /></button>
-            </div>
-                <img src={ ImgApp } alt="imagem bonitinha "></img>
-            <div>
-                <RiMediumLine />
-                <RiLinkedinBoxLine />
-                <RiInstagramLine />
-            </div>
-        </Fragment>
-        )
-    }
+  const [usuario, setUsuario ] = useState('')
+  return (
+    <Fragment>
+      <S.Container>
+        <div>
+          <S.ImgDiv src={ImgBack} alt="imagem do backgroud"></S.ImgDiv>
+        </div>
+        <div>
+          <S.LogoBit src={Logo} alt="logo da Bitwase"></S.LogoBit>
+        </div>
+        <div>
+          <S.Title>Buscar um usuário no <b>Github</b> ficou muito <b>fácil!</b></S.Title>
+          <S.Input placeholder="Buscar usuário" className="usuarioInput" value={usuario} onChange={e => console.log(e.target.value)} ></S.Input>
+          <RiSearchLine />
+          <S.Button type="button"><RiGithubLine /></S.Button>
+        </div>
+        <S.ImgApp src={ImgApp} alt="imagem bonitinha "></S.ImgApp>
+        <S.DivIcon>
+          <RiMediumLine />
+          <RiLinkedinBoxLine />
+          <RiInstagramLine />
+        </S.DivIcon>
+      </S.Container>
+    </Fragment>
+  )
+
 }
 
-export default graphql(getRepositories, {name: 'repositories'})(handlePesquisa)
+export default graphql(getRepositories, { name: 'repositories' })(App)
---
+++ bitwase-teste/src/Pages/Home/styled.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0px;
+    top: 0px;
+    background: linear-gradient(359.39deg, #EFEEEE 0.46%, #FFFFFF 99.42%);
+
+`
+export const ImgDiv = styled.div`
+    /* position: absolute;
+    width: 925px;
+    height: 647px;
+    left: 515px;
+    top: 0px; */
+`
+
+export const LogoBit = styled.img`
+    position: absolute;
+    left: 10.42%;
+    right: 80.33%;
+    top: 7.73%;
+    bottom: 86.08%;
+`
+export const Input = styled.input`
+    position: absolute;
+    width: 563px;
+    height: 56px;
+    left: 150px;
+    top: 436px;
+    background: #FFFFFF;
+    border: 1px solid #FFFFFF;
+    box-sizing: border-box;
+    border-radius: 4px;
+    box-shadow: 0px 4px 4px grey;
+`
+export const Button = styled.button`
+    position: absolute;
+    width: 64px;
+    height: 56px;
+    left: 649px;
+    top: 436px;
+    background: #5A3D5C;
+    border-radius: 2px;
+    color: #fff;
+    border: 0px;
+`
+export const ImgApp = styled.img`
+   position: absolute;
+    width: 443px;
+    height: 406px;
+    left: 843px;
+    top: 164px;
+`
+
+
+export const Title = styled.h1`
+    position: absolute;
+    width: 540px;
+    height: 100px;
+    left: 150px;
+    top: 249px;
+    font-size: 42px;
+    line-height: 50px;
+    letter-spacing: -3px;
+    color: #5a3d5c;
+`
+export const DivIcon = styled.div`
+    left: 49.21%;
+    right: 46.74%;
+    top: 91.62%;
+    bottom: 5.28%;
+    color: #fd71ea;
+`
