5eb256bf32b974f06d731f8a71cf8b9f6f0bef3e
Tiago José <tiago_jvo@outlook.com>
2020-08-14 19:17:48 -0300

    feat: add spinner component

--- bitwise/src/App.tsx
+++ bitwise/src/App.tsx
@@ -1,21 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, lazy, Suspense } from 'react';
 
-import './styles/main.scss';
-
-import Landing from './pages/Landing';
-import Profile from './pages/Profile'
+import Spinner from './components/Spinner';
 
 import { UserContext } from './contexts/UserContext';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
+
+import './styles/main.scss';
+
+const Landing = lazy(() => import('./pages/Landing'));
+const Profile = lazy(() => import('./pages/Profile'));
+
+
 const App = () => {
 	const userContext: any = useContext(UserContext);
 
 	return (
 		<div>
 			<Switch>
-				<Route exact path="/" component={Landing} />
-				<Route path="/profile" render={() => userContext.data ? <Profile /> : <Redirect to="/" />} />
+				<Suspense fallback={<Spinner />}>
+					<Route exact path="/" component={Landing} />
+					<Route path="/profile" render={() => (userContext.data ? <Profile /> : <Redirect to="/" />)} />
+				</Suspense>
 			</Switch>
 		</div>
 	);
---
+++ bitwise/src/components/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './styles.scss'
+const Spinner = () => {
+    return (
+        <div className='spiner-container'>
+            <span className='spiner-container__spinner'></span>
+        </div>
+    )
+}
+
+export default Spinner
