de50c08a3cc2b466832e69b48cf3b0d218cf7aa4
Laio Andrade <laioand98@gmail.com>
2020-11-26 20:54:36 -0300

    Add ApolloClient to project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache()
+});
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
