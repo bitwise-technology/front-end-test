a1e9bd780f67b3323351d7a0158f9bb8bab884f5
Laio Andrade <laioand98@gmail.com>
2020-11-28 15:28:47 -0300

    add handlers and state to user search

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import classes from './Home.module.css';
 
@@ -8,7 +8,40 @@ import SearchBar from '../../components/searchBar/SearchBar';
 import bgImage from '../../assets/images/background-image.svg';
 import homeImage from '../../assets/images/home-image.svg';
 
+import { useQuery, gql } from '@apollo/client';
+
+const GET_USER = gql`
+  query  User($login: String!){
+    user(login: $login) {
+      name,
+      avatarUrl
+    }
+  }
+`;
+
+
+
+const Users = ({login}) => {
+  const { data } = useQuery(GET_USER, ({ variables: { login }}));
+  return (
+    data ? <p>{data.user.repositories.nodes.map(repository => repository.defaultBranchRef.target.history.edges[0].node.message)} </p> : null
+  )
+}
+
+
 const Home = () => {
+  const [login, setLogin] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+
+  const inputChangeHandler = (event) => {
+    setLogin(event.target.value);
+  }
+
+  const clickHandler = () => {
+    setIsClicked(true);
+    
+  }
+  
   return (
     <div className={classes.Container}>
       <Logo />
@@ -18,7 +51,8 @@ const Home = () => {
           <h1 className={classes.HomeText}>
             Buscar um usuário no <strong>Github </strong>ficou muito <strong>fácil!</strong>
           </h1>
-          <SearchBar />
+          <SearchBar changeHandler={inputChangeHandler} clicked={clickHandler}/>
+          {isClicked ? <Users login={login}/> : null}
         </div>
         <img src={homeImage} alt="Bitwise" />
       </div>
