19be75555590295973c96aaecd5dc7a0afb7c88b
Laio Andrade <laioand98@gmail.com>
2020-11-30 17:42:51 -0300

    add authorization to ApolloClient

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,13 @@ import './index.css';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const token = "931218e4775c658c1e9501186f7d4f9fba6b2352";
+
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
   cache: new InMemoryCache()
 });
 
