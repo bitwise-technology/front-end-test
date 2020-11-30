b10d9d3de2ccdc7b1da52f50d017686b574e256d
Laio Andrade <laioand98@gmail.com>
2020-11-30 17:41:57 -0300

    add Medias component

---
+++ frontend/src/components/medias/Medias.js
@@ -0,0 +1,10 @@
+import sociais from '../../assets/images/sociais.svg';
+import classes from './Medias.module.css';
+
+const Medias = () => {
+  return (
+      <img src={sociais} alt="Medium Linkedin Instagram"/>
+  )
+}
+
+export default Medias;
