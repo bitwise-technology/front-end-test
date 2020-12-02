fa124e1fc03bc2239c6497cb6b1c51cbd717a917
Laio Andrade <laioand98@gmail.com>
2020-12-02 19:18:26 -0300

    change token

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const token = "931218e4775c658c1e9501186f7d4f9fba6b2352";
+const token = process.env.REACT_APP_TOKEN_API;
 
 const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
