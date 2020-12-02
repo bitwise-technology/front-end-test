2f537f51a4a9b70132fb503300aa89b3e35511f6
Laio Andrade <laioand98@gmail.com>
2020-12-02 14:52:49 -0300

    add Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}/> 
+        <Route path="/search" component={Search}/> 
+      </Switch>
+      
+    </Router>
+
+    )
+    
+      
+
 }
 
 export default App;
