22a3a23f6e57acb05878502911d3fe15ee819ba5
Tiago José <tiago_jvo@outlook.com>
2020-12-03 12:52:11 -0300

    chore: add react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
+
+import {Switch , Route} from 'react-router-dom';
+
 import Home from "./pages/home/HomePage";
+import SearchPage from "./pages/search/SearchPage";
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
-      <Home />
+      <Switch>
+        <Route exact path='/' component={Home}/>  
+        <Route path='/search' component={SearchPage} />
+      </Switch>
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://api.github.com/graphql",
   headers: {
-    'Authorization' : `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
   },
   cache: new InMemoryCache(),
 });
@@ -19,7 +17,9 @@ const client = new ApolloClient({
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
