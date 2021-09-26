ed31c9fa73c4d9c5bc80027463213a3923b1e92d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-09-26 20:44:49 -0300

    Create background component

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Polygon from '../../svg/Polygon 1.svg'
+import Polygon from '../../svg/Polygon1.svg'
 import Backgroundimage from './styled'
 
 const Background = () => {
   const descricao= 'Aqui é um background'
   return (
-    <React.Fragment>
+    <>
       <Backgroundimage>
         <img src={Polygon} alt={descricao} />
       </Backgroundimage>
-    </React.Fragment>
+    </>
   )
 }
 
