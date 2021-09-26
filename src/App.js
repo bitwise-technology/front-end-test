6352d477a7c291248bd9c2697619ecef2b420125
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-09-26 20:43:48 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,94 @@
-import React from 'react'
-import Background from './Components/Background/Background';
-import Container  from './Components/Container/Container';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import useDebounce from "./hooks/useDebounce";
+import { gql, GraphQLClient } from "graphql-request";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import Dashboard from "./Pages/Dashborad/Dashoborad.jsx";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const endpoint = "https://api.github.com/graphql";
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
+
+  function useInfo(userName) {
+    return useQuery(["info", userName], async () => {
+      const { repositoryOwner: data } = await graphQLClient.request(
+        gql`
+            query {
+              repositoryOwner(login: ${JSON.stringify(userName)}) {
+                ... on User {
+                  name
+                  avatarUrl
+                  repositories(first: 100) {
+                    repositoryCount: totalCount
+                    list: nodes {
+                      name
+                      ... on Repository {
+                        refs(first: 1, refPrefix: "refs/heads/") {
+                          edges {
+                            repo: node {
+                              branch: name
+                              target {
+                                ... on Commit {
+                                  commits: history(first: 1) {
+                                    totalCommits: totalCount
+                                    lastCommit: edges {
+                                      node {
+                                        hash: oid
+                                        title: messageHeadline
+                                      }
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          `
+      );
+
+      if (data !== null) {
+        return data;
+      } else {
+        throw new Error("user not found");
+      }
+    });
+  }
+
+  const debouncedSearchString = useDebounce(username, 1000);
+  const { data, isLoading, error } = useInfo(debouncedSearchString);
   return (
-    <React.Fragment>
-      <Background />
-      <Container />
-    </React.Fragment>
+    <Routes>
+      <Route
+        path="/"
+        element={<Home userName={username} updateUserName={setUsername} />}
+      />
+      <Route
+        path="Dashboard"
+        element={
+          <Dashboard
+            userName={username}
+            data={data}
+            isLoading={isLoading}
+            updateUserName={setUsername}
+            error={error}
+          />
+        }
+      />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './reset.css'
+import React from "react";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { createBrowserHistory } from "history";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./reset.css";
+
+
+const queryClient = new QueryClient();
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter history={history}>
+      <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
