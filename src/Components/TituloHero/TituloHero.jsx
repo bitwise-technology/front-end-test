e6645fa2796870c8c46c3ecc9f7c64e2a1d4f2b6
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-09-26 21:00:45 -0300

    Create TituloHero component

--- src/Components/TituloHero/TituloHero.jsx
+++ src/Components/TituloHero/TituloHero.jsx
@@ -3,11 +3,11 @@ import TituloHeroh2 from './styled'
 
 const TituloHero = () => {
   return (
-    <React.Fragment>
+    <>
       <TituloHeroh2>
         Buscar um usuário no <span style={{fontWeight: 'bold'}}> Github </span> ficou muito <span style={{fontWeight: 'bold'}}>fácil!</span>
       </TituloHeroh2>
-    </React.Fragment>
+    </>
   )
 }
 
