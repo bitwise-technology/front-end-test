15d6cc82f7dd1d83d7877dbfea8227dfc07217bd
Maikon Camilo Gosch <maikongosch@hotmail.com>
2022-04-28 23:57:13 -0300

    Layout padrão da aplicação em desenvolvimento

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app class="LayoutDefault">
+    <div class="bg">
+      <nuxt />
+      <div v-if="$route.query.user">
+        <Footer />
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Footer,
+  },
+}
+</script>
+
+<style scoped>
+.LayoutDefault .bg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+  background: linear-gradient(359.39deg, #EFEEEE 0.46%, #FFFFFF 99.42%);
+}
+
+</style>
