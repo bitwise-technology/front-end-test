ff3828b72c68000033311d4b60947b9c2a10b6b2
Maikon Camilo Gosch <maikongosch@hotmail.com>
2022-04-28 23:51:18 -0300

    Componente de midias socias em desenvolvimento

---
+++ components/SocialMediaIcons.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="SocialMediaIcons d-inline-flex">
+      <div>
+        <img height="25" width="25" alt="medium" src="medium.svg" />
+      </div>
+      <div>
+        <img height="25" width="25" alt="linkedin" src="linkedin.svg" />
+      </div>
+      <div>
+        <img height="25" width="25" alt="instagram" src="instagram.svg" />
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.SocialMediaIcons img {
+  color: #FD71EA;
+  margin-right: 12px;
+}
+
+</style>
