843915946b068936417732b9a7963706ed6df289
Maikon Camilo Gosch <maikongosch@hotmail.com>
2022-04-29 13:47:50 -0300

    Alerta responsivo

--- components/Alert.vue
+++ components/Alert.vue
@@ -8,22 +8,22 @@
     >
       <v-container class="Alert">
         <v-row class="text-right">
-          <v-col>
+          <v-col cols="12">
             <v-btn icon dark @click="dialog = false">
               <v-icon small>mdi-close</v-icon>
             </v-btn>
           </v-col>
         </v-row>
         <v-row class="mt-n6 ml-5">
-          <v-col>
+          <v-col cols="12">
             <span>{{ message }}</span>
           </v-col>
         </v-row>
         <v-row class="social-media">
-          <v-col>
+          <v-col cols="12">
             <label> Enquanto isso, acompanhe a Bitwise nas redes sociais: </label>
           </v-col>
-          <v-col class="mr-n12">
+          <v-col cols="12" class="mr-n12">
             <SocialMediaIcons :color-white="true" />
           </v-col>
         </v-row>
@@ -68,14 +68,14 @@ export default {
 .Alert {
   background: linear-gradient(90deg, #A51C92 0%, #F638DC 100%);
   border-radius: 20px !important;
-  width: 800px;
+  width: 350px;
   margin-top: 60px;
 }
 .Alert span {
   font-family: 'Gotham-Bold';
   font-style: normal;
   font-weight: 700;
-  font-size: 36px;
+  font-size: 20px;
   line-height: 40px;
   display: flex;
   align-items: center;
@@ -87,7 +87,7 @@ export default {
   font-family: 'Futura Bk BT';
   font-style: normal;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 11px;
   line-height: 40px;
   display: flex;
   align-items: center;
@@ -95,7 +95,26 @@ export default {
 }
 .Alert .social-media {
   margin: 16px 0 8px 20px;
-  align-items: center;
+  text-align: center;
+}
+
+@media (min-width: 600px) {
+  .Alert {
+    background: linear-gradient(90deg, #A51C92 0%, #F638DC 100%);
+    border-radius: 20px !important;
+    width: 800px;
+    margin-top: 60px;
+  }
+  .Alert span {
+    font-size: 36px;
+  }
+  .Alert label {
+    font-size: 14px;
+  }
+  .Alert .social-media {
+    margin: 16px 0 8px 20px;
+    align-items: center;
+  }
 }
 
 </style>
