11be6ef6e1a28f6b5361d15307b6b04638e60615
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2022-08-17 22:29:42 -0300

    Adding global styles in the App file

--- front-teste/src/App.tsx
+++ front-teste/src/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
