4a4417fc73971d23c9c7c1494db481d29c271f12
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2022-08-17 22:03:01 -0300

    Adding logo component

---
+++ front-teste/src/components/Logo/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Logo } from './styles';
+
+const BitwiseLogo: React.FC = () => {
+  return (
+    <a href="https://bitwise.ltda/">
+      <Logo />
+    </a>
+  );
+};
+
+export default BitwiseLogo;
---
+++ front-teste/src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import logo from '../../assets/images/logo-bitwise.png';
+
+export const Logo = styled.img.attrs({
+  src: logo,
+  alt: 'logo-bitwise',
+})``;
