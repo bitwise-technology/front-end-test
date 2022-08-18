acbd2eccf198dc422e2d53dbfe6ca4c70356526e
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2022-08-17 22:52:33 -0300

    Adding routes in the App file

--- front-teste/src/App.tsx
+++ front-teste/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
+    <Routes />
     <GlobalStyle />
   </>
 );
