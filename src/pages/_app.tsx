021c807dc2605367e359fd4e739ab57d10010ab2
ZauJulio <zaujulio.dev@gmail.com>
2023-02-14 23:49:15 -0300

    refactor(styles): load new styles on next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import '../styles/global.css'
+
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
-import { theme } from '@/styles/theme'
+import { theme } from '@/styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
   )
