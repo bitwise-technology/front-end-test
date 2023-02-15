c80664bcf87a52c3caa6590265ba55755b8a68f9
ZauJulio <zaujulio.dev@gmail.com>
2023-02-14 23:48:59 -0300

    refactor(components): add index export to components module

---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+export { SearchBar } from './SearchBar'
+export { DataTable } from './DataTable'
+
+export { Logo } from './Logo'
+export { SocialIcons } from './SocialIcons'
+export { MediumIcon } from './MediumIcon'
