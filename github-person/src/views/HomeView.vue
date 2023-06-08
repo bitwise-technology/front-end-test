2942b229e1e2d654a51f2ede6277e57e8fd54977
JFmaia <jfmaia741@gmail.com>
2023-06-08 10:59:26 -0300

    style: Modified background

--- github-person/src/views/HomeView.vue
+++ github-person/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="home-container">
+        <section class="container home">
+            <h1>Olá</h1>
+        </section>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    
 }
 </script>
+
+<style scoped>
+    #home-container{
+        background-image: url('@/assets/background.svg');
+        background-size: cover;
+        font-family: 'Gotham', sans-serif;
+    }
+
+    .home{
+        padding: 60px 150px 41px 150px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+</style>
