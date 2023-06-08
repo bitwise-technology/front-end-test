0f49ecd1e680bb0c411cb8fdeee86b4136099825
JFmaia <jfmaia741@gmail.com>
2023-06-08 11:02:18 -0300

    feat: Mounting home screen

--- github-person/src/views/HomeView.vue
+++ github-person/src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <template>
     <div id="home-container">
-        <section class="container home">
-            <h1>Olá</h1>
+        <section class="container home" id="home" name="home">
+            <img class="logotipo" src="@/assets/logo.svg" alt="Logo da Bitwise">
+            <h1 class="title">Buscar um usuário no <strong>Github</strong> ficou muito <strong>fácil!</strong></h1>
+            <InputSearch></InputSearch>
+            <SocialContainer></SocialContainer>
         </section>
     </div>
 </template>
 
 <script>
+import InputSearch from '@/components/home/InputSearch.vue';
+import SocialContainer from '@/components/home/SocialContainer.vue';
 export default {
-    
+    components:{InputSearch, SocialContainer},
 }
 </script>
 
@@ -17,6 +22,7 @@ export default {
         background-image: url('@/assets/background.svg');
         background-size: cover;
         font-family: 'Gotham', sans-serif;
+        height: 100%;
     }
 
     .home{
@@ -24,5 +30,25 @@ export default {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
+        justify-content: center;
+        height: 100%;
+    }
+
+    .logotipo{
+        margin-bottom: 133px;
+    }
+
+    .title{
+        margin-bottom: 91px;
+        font-size: 2.625rem;
+        max-width: 540px;
+        line-height: 3rem;
+        letter-spacing: -3px;
+        font-weight: 400;
+        color: #42163A;
+    }
+
+    .title strong{
+        font-weight: 700;
     }
 </style>
