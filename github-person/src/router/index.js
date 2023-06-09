923a598e89ef12d9a092bd3f3ef86e8ead949fd7
JFmaia <jfmaia741@gmail.com>
2023-06-09 02:28:05 -0300

    feat: Add new route

--- github-person/src/router/index.js
+++ github-person/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SearchView from '../views/SearchView.vue'
 
 const routes = [
   {
@@ -7,6 +8,12 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path:'/search',
+    name: 'search',
+    component: SearchView,
+    props:true
+  }
 ]
 
 const router = createRouter({
