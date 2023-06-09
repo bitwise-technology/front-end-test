5bbb6947d63f80484fd423a013c8851ff685261b
JFmaia <jfmaia741@gmail.com>
2023-06-09 00:34:50 -0300

    feat: ADD responsive layout

--- github-person/src/views/HomeView.vue
+++ github-person/src/views/HomeView.vue
@@ -2,8 +2,10 @@
     <div id="home-container">
         <section class="container home" id="home" name="home">
             <img class="logotipo" src="@/assets/logo.svg" alt="Logo da Bitwise">
-            <h1 class="title">Buscar um usuário no <strong>Github</strong> ficou muito <strong>fácil!</strong></h1>
-            <InputSearch></InputSearch>
+            <div>
+                <h1 class="title">Buscar um usuário no <strong>Github</strong> ficou muito <strong>fácil!</strong></h1>
+                <InputSearch></InputSearch>
+            </div>
             <SocialContainer></SocialContainer>
         </section>
     </div>
@@ -19,7 +21,7 @@ export default {
 
 <style scoped>
     #home-container{
-        background-image: url('@/assets/background.svg');
+        background-image: url('@/assets/backgrounds/background.svg');
         background-size: cover;
         font-family: 'Gotham', sans-serif;
         padding: 60px 150px 41px 150px;
@@ -51,4 +53,50 @@ export default {
     .title strong{
         font-weight: 700;
     }
+
+    /* Responsive */
+
+    @media (max-width:860px){
+        #home-container{
+            padding: 60px 40px 41px 40px;
+            height: 100%;
+        }
+    }
+
+    @media (max-width:640px){
+        .title{
+            font-size: 2.3rem;
+        }
+    }
+
+    @media (max-width:480px){
+        #home-container{
+            background-image: url('@/assets/backgrounds/min-background.svg');
+            background-size: cover;
+            padding: 32px 16px 16px 16px;
+        }
+        .home{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: space-between;
+            height: 100%;
+        }
+
+        .logotipo{
+            margin-bottom: 0px;
+        }
+
+        .title{
+            margin-bottom: 40px;
+            font-size: 2.625rem;
+            max-width: 320px;
+            line-height: 2rem;
+            letter-spacing: -1px;
+            font-weight: 400;
+            line-height: 40px;
+            color: #42163A;
+        }
+    }
+
 </style>
