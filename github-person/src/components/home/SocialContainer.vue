401c23c3fb997abf1eb1f47aac839891277843cf
JFmaia <jfmaia741@gmail.com>
2023-06-09 00:34:36 -0300

    feat: ADD responsive layout

--- github-person/src/components/home/SocialContainer.vue
+++ github-person/src/components/home/SocialContainer.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="social-container">
-        <a href="http://" target="_blank" rel="Link para o email">
-            <img src="@/assets/email.png" alt="Icone do Email" height="24" width="24">
+        <a href="" target="_blank" rel="Link para o email">
+            <img src="@/assets/social/email.png" alt="Icone do Email" height="24" width="24">
         </a>
-        <a href="http://" target="_blank" rel="Link para o LinkedIn">
-            <img src="@/assets/linkedin.png" alt="Icone do LinkedIn" height="24" width="24">
+        <a href="https://www.linkedin.com/company/bitwisetechnology/" target="_blank" rel="Link para o LinkedIn">
+            <img src="@/assets/social/linkedin.png" alt="Icone do LinkedIn" height="24" width="24">
         </a>
-        <a href="http://" target="_blank" rel="Link para o Instagram">
-            <img src="@/assets/instagram.png" alt="Icone do Instagram" height="24" width="24">
+        <a href="https://www.instagram.com/bitwisetechnology/" target="_blank" rel="Link para o Instagram">
+            <img src="@/assets/social/instagram.png" alt="Icone do Instagram" height="24" width="24">
         </a>
     </div>
 </template>
