efd04a4f16a4f9e851c3ae36e65742997554a204
JFmaia <jfmaia741@gmail.com>
2023-06-10 14:51:49 -0300

    feat: Modified localy of components

---
+++ github-person/src/components/SocialContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="social-container">
+        <a href="" target="_blank" rel="Link para o email">
+            <img src="@/assets/social/email.png" alt="Icone do Email" height="24" width="24">
+        </a>
+        <a href="https://www.linkedin.com/company/bitwisetechnology/" target="_blank" rel="Link para o LinkedIn">
+            <img src="@/assets/social/linkedin.png" alt="Icone do LinkedIn" height="24" width="24">
+        </a>
+        <a href="https://www.instagram.com/bitwisetechnology/" target="_blank" rel="Link para o Instagram">
+            <img src="@/assets/social/instagram.png" alt="Icone do Instagram" height="24" width="24">
+        </a>
+    </div>
+</template>
+<style scoped>
+    .social-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 11px;
+    }
+</style>
