662567fdca3afec5565971151ff05103efca1a18
virginiamaia <virginiamaiabf@gmail.com>
2023-06-12 21:58:34 -0300

    Deleting unnecessary codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,43 +17,6 @@ import toast, { Toaster } from "react-hot-toast";
 import { X } from "@phosphor-icons/react";
 import ToastCustom from "@/components/ToastCustom";
 
-// export const ToastCustom = ({ onCloseToast }: { onCloseToast: () => void }) => {
-//   return (
-//     <div
-//       style={{
-//         background: "#5B1E50",
-//         padding: 40,
-//         display: "flex",
-//         borderRadius: 8,
-//         minWidth: 700,
-//         color: "white",
-//       }}
-//     >
-//       <div
-//         style={{
-//           flex: 1,
-//           display: "flex",
-//           flexDirection: "column",
-//           justifyContent: "space-between",
-//         }}
-//       >
-//         <h2 style={{ fontWeight: 300 }}>Nenhum usuário encontrado!</h2>
-//         <p style={{ marginTop: 16 }}>
-//           Enquanto isso, acompanhe a Bitwise nas redes sociais:
-//           <Image alt="" src={vector3} style={{marginLeft: 8, marginRight: 8}}/>
-//           <Image alt="" src={vector4} />
-//         </p>
-//       </div>
-//       <button
-//         style={{ border: 0, background: "transparent", cursor: "pointer" }}
-//         onClick={onCloseToast}
-//       >
-//         <X size={24} color="white" />
-//       </button>
-//     </div>
-//   );
-// };
-
 export default function Home() {
   const [search, setSearch] = useState("");
   const router = useRouter();
