c4c4b570d5f893b245ff18000bc8bf4657fa118f
Rcsn03 <rafael.csn@hotmail.com>
2024-02-22 05:28:55 -0300

    adjust

--- js/app.js
+++ js/app.js
@@ -30,6 +30,78 @@ x.addEventListener("click", () => {
   errorDiv.classList.add("inactive");
 });
 
+fetch('https://api.github.com/users')
+    .then(res => res.json())
+    .then((json) => {
+        console.log(json);
+        const ul = document.getElementById('listaProdutos');
+        json.forEach((item) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+            
+                <span>${item.login}</span>
+            
+            `;
+            ul.appendChild(li);
+        })
+    })
+
+function filtrar() {
+  var input,
+      filter,
+      ul,
+      li,
+      a,
+      i,
+      span,
+      txtValue,
+      count = 0
+
+  //PEGAR OS ELEMENTOS HTML
+  input = document.getElementById('search');
+  ul = document.getElementById('listaProdutos');
+
+  //FILTRO
+  filter = input.value;
+
+  //PEGAR TODAS AS LI's DA LISTA
+  li = ul.getElementsByTagName("li");
+
+  //PERCORRER TODOS OS LI'S
+  for (i = 0; i < li.length; i++) {
+      //PEGAR A TAG A DO ELEMENTO PERCORRIDO
+      a = li[i].getElementsByTagName("a")[0];
+      //PEGAR O TEXTO DENTRO DO NOSSA TAG A
+      txtValue = a ;
+      //VERIFICAR SE O QUE O USUARIO DIGITOU BATE COM O TEXTO DA TAG A
+      if (txtValue .indexOf > -1) {
+          //VALOR BATEU
+          li[i].style.display = "";
+          //INCREMENTAR O CONTADOR
+          count++
+          //PEGAR A TAG SPAN DO ITEM
+          span = li[i].querySelector(".item-name");
+          //SE EXISTIR
+          if (span) {
+              span.innerHTML = txtValue.replace(new RegExp(filter, "gi"), (match) => {
+                  return "<strong>" + match + "</strong>";
+              })
+          }
+      } else {
+          //NÃO MOSTRA O ITEM DA LISTA
+          li[i].style.display = "none";
+      }
+  }
+  
+//VERIFICANDO SE TEM ITENS NA LISTA
+    if(filter ===""){ 
+  ul.style.display = "none";
+  }else{ 
+  ul.style.display = "block";
+  }
+}
+
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   loading.style.display = "block";
@@ -109,10 +181,9 @@ form.addEventListener("submit", async (e) => {
           let data = await res.json();
           data.map( item => {
             
-            let soma = 0; 
-            for (let i = 1; i <= item.contributions.length; i++) { 
-              soma += item.contributions;
-            }                         
+            for (i = 0; i <= item.length; i++) {            
+              soma += item.contributions;               
+            }                                               
             
             const qtdcommit = document.createElement("span")
             qtdcommit.classList.add("qtd_commit")
